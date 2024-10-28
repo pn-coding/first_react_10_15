768beacd10ffee82bfbd3a5eb98bd47e1d7e18d7
pn-coding <pncoding@naver.com>
2024-10-28 13:17:13 +0900

    router Ex

--- src/components/ex07/pages/Sub01.js
+++ src/components/ex07/pages/Sub01.js
@@ -1,5 +1,30 @@
+import { useParams } from "react-router-dom";
+
+const subPageData = [
+  {
+    id: 0,
+    title: "SUB 01",
+    desc: "첫번째 서브페이지",
+  },
+  {
+    id: 1,
+    title: "SUB 02",
+    desc: "두번째 서브페이지",
+  },
+];
+
 const Sub01 = () => {
-  return <div>SUB 01</div>;
+  const { id } = useParams();
+  // =>router 경로 중 변수값을 얻어옴
+  // =>객체로 반환됨
+  // =>객체 비구조화 할당 이용할수 있음
+
+  return (
+    <div>
+      <h2>{subPageData[id].title}</h2>
+      <p>{subPageData[id].desc}</p>
+    </div>
+  );
 };
 
 export default Sub01;
